5c00ae769bd182d3a475925e1439a2907bf74a6e
katarinamarj <katarinamarjanovic02@gmail.com>
2025-03-08 17:06:20 +0100

    Modified App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Register from "./pages/Register";
 
 
 function App() {
-  const [setMessage] = useState("");
+  const [message, setMessage] = useState("");
 
   useEffect(() => {
     fetch("https://127.0.0.1:8000/api/message")
